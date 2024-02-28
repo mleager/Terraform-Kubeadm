ea49e27a0d789b608bb4eb81500603859098d36e
Mark <markleager92@gmail.com>
2024-02-28 00:02:20 -0500

    Add basic variables

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "project" {
+  default     = "k8s"
+  type        = string
+  description = "Project Name. Default is [ k8s ]"
+}
+
+variable "region" {
+  default     = "us-east-1"
+  type        = string
+  description = "AWS Region. Default is [ us-east-1 ]"
+}
+
+variable "instance_type" {
+  default     = "t2.micro"
+  type        = string
+  description = "EC2 Instance Type. Default is [ t2.micro ]"
+}
+
+variable "user_data" {
+  default     = "scripts/1-ubuntu.sh"
+  type        = string
+  description = "User Data script location. Default is [ scripts/1-ubuntu.sh ]"
+}
